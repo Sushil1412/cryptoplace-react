18a22909ce1fac6f69510af28d2e22b016e62d5c
Sushil1412 <you@example.com>
2024-06-27 08:47:54 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ import CoinContextProvider from './context/CoinContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cryptoplace-react">
       <CoinContextProvider>
         <App />
       </CoinContextProvider>
