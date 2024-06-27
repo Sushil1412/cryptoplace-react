64848fc643de0ae12aff91886255d3e3f6b813dc
Sushil1412 <you@example.com>
2024-06-27 18:23:56 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, HashRouter, RouterProvider } from 'react-router-dom'
 import Coin from './pages/Coin/Coin.jsx'
 import Home from './pages/Home/Home';
 import CoinContextProvider from './context/CoinContext.jsx'
@@ -25,12 +25,12 @@ import CoinContextProvider from './context/CoinContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cryptoplace-react">
+    <HashRouter>
       <CoinContextProvider>
         <App />
       </CoinContextProvider>
 
-    </BrowserRouter>
+    </HashRouter>
 
 
 
